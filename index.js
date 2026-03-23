fb3ddd88d6f080da4e72ccee177092155ab8372f
reyhanfeyzaakay <reyhanfeyzaakay@gmail.com>
2026-03-23 23:24:51 +0300

    Create index.js as the entry point for the React Native application

---
+++ index.js
@@ -0,0 +1,8 @@
+// index.js
+// Entry point for the React Native Application
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
